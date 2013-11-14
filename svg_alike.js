cbb4f462540d7144743c0a5dfa538c3a78321d88
Nicholas Johnson <nicholas@forwardadvance.com>
2013-11-14 22:21:01 +0000

    Changed jpg to png.

--- svg_alike.js
+++ svg_alike.js
@@ -2,14 +2,14 @@
   Self contained SVG polyfill
 
   SVG is awesome. You get double density out of the box, you can pinch to zoom without pixelation, but what about IE8?
-  SVG-alike checks for SVG support, and if it doesn't find it, swaps in a JPG instead.
+  SVG-alike checks for SVG support, and if it doesn't find it, swaps in a PNG instead.
 
   Usage.
-  1. Save your images in both svg and jpg format, and place them side by side.
+  1. Save your images in both svg and png format, and place them side by side.
   2. Import JQuery <script src="jQuery.js"></script>
   3. Import SVG-alike <script src="svg_alike.js"></script>
   4. Pop your SVG on the page using an img tag, like so: <img src="awesome.svg" />
-  5. On older browsers, SVG-alike will rewrite your image tag like so: <img src="awesome.jpg" />
+  5. On older browsers, SVG-alike will rewrite your image tag like so: <img src="awesome.png" />
 */
 
 (function($) {
@@ -21,8 +21,9 @@
     }
     if (!svgSupport()) {
       this.find('img[src^=".svg"').each(function() {
-        $(this).attr('src', img.attr('src').replace(".svg", ".jpg"));
+        $(this).attr('src', img.attr('src').replace(".svg", ".png"));
       })
     }
+    return self;
   }
 })(jQuery);
